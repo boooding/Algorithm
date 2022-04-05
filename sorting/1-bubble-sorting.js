514247617271e0c362cb4bdd164e5373e3fecb0f
boooding <boooding.tang@outlook.com>
2022-04-05 23:31:40 +0800

    update

---
+++ sorting/1-bubble-sorting.js
@@ -0,0 +1,7 @@
+const bubbleSorting = (array) => {
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length - 1 - i; j++) {
+            if (array[j]) {}
+        }
+    }
+}
