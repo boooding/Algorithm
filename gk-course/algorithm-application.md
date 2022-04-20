4f94857e1218e50d8d9d10cdb665833967d56aae
boooding <boooding.tang@outlook.com>
2022-04-20 11:30:12 +0800

    update

--- gk-course/algorithm-application.md
+++
@@ -1,5 +0,0 @@
-## LRU Cache Linked list
-1. https://leetcode-cn.com/problems/lru-cache/Redis Skip List
-
-## Redis Skip List
-1. https://www.zhihu.com/question/20202931
--- old/Codetop_front/88_MergeSortedArray.js
+++
@@ -1,6 +0,0 @@
-
-let merge1 = (number1, m, number2, n) => {
-    number2.splice(n, number2.length - n);
-    number1.splice(m, number1.length - m, ...number2)
-    number1.sort((a, b) => a - b)
-}
